875727fe5cd04a0bd0ab6a7aa9498a7668fb0d86
daniel-mosko <danielmosko5@gmail.com>
2025-06-23 12:48:06 +0200

    refactor: handle empty files case and simplify the logic

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,12 @@
 use serde_json::json;
-use serde::{Deserialize, Serialize};
 use std::error::Error;
 
 
-use crate::git::{diff, get_changed_files, open_repo};
+use crate::{git::{diff, get_changed_files, open_repo}, schemas::{ChatResponse, UserMessage}};
 
 mod git;
 
-#[derive(Deserialize)]
-struct ChatResponse {
-    choices: Vec<ChatChoice>,
-}
-
-#[derive(Deserialize)]
-struct ChatChoice {
-    message: ChatMessage,
-}
-
-#[derive(Deserialize)]
-struct ChatMessage {
-    content: String,
-}
-
-#[derive(Serialize)] // to be able to convert to JSON
-struct Message {
-    role: String,
-    content: String,
-}
+mod schemas;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -35,69 +15,70 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let mut files: Vec<String> = Vec::new();
 
-    match get_changed_files(&repo) {
-        Ok(changed_files) => {
-            for file in changed_files {
-                files.push(file.display().to_string());
-            }
-        }
-        Err(e) => eprintln!("Error: {}", e),
-    }
+    get_changed_files(&repo)
+        .iter()
+        .for_each(|path_bufs| {
+            path_bufs.iter().for_each(|changed_file| {
+                files.push(changed_file.display().to_string());
+            });
+        });
 
-    let system_msg = Message {
-        role: "system".to_string(),
-        content: 
-            "You are an AI assistant that generates concise, short and clear Git commit messages from code diffs: \n".to_string(),
-    };
+    if !files.is_empty() {
+        let system_msg = UserMessage {
+            role: "system".to_string(),
+            content: 
+                "You are an AI assistant that generates concise, short and clear Git commit messages from code diffs: \n".to_string(),
+        };
 
-    let send_msg = Message {
-        role: "user".to_string(),
-        content: diff(&repo, &files).unwrap(),
-    };
+        let send_msg = UserMessage {
+            role: "user".to_string(),
+            content: diff(&repo, &files).unwrap(),
+        };
 
-    let mut messages: Vec<Message> = vec![];
-    messages.push(system_msg);
-    messages.push(send_msg);
+        let mut messages: Vec<UserMessage> = vec![];
+        messages.push(system_msg);
+        messages.push(send_msg);
 
-    let request_payload = json!({
-        "model": "{{model}}",
-        "messages": messages,
-        "response_format": {
-            "type": "json_schema",
-            "json_schema": {
-                "name": "commit_response",
-                "strict": "true",
-                "schema": {
-                    "type": "object",
-                    "properties": {
-                        "message": {
-                            "type": "string"
-                        }
-                    },
-                    "required": ["message"]
+        let request_payload = json!({
+            "model": "{{model}}",
+            "messages": messages,
+            "response_format": {
+                "type": "json_schema",
+                "json_schema": {
+                    "name": "commit_response",
+                    "strict": "true",
+                    "schema": {
+                        "type": "object",
+                        "properties": {
+                            "message": {
+                                "type": "string"
+                            }
+                        },
+                        "required": ["message"]
+                    }
                 }
-            }
-        },
-        "temperature": 0.7,
-        "max_tokens": 50,
-        "stream": false
-    });
+            },
+            "temperature": 0.7,
+            "max_tokens": 50,
+            "stream": false
+        });
 
-    // println!("Sending JSON:\n{}", serde_json::to_string_pretty(&request_payload).unwrap());
+        // println!("Sending JSON:\n{}", serde_json::to_string_pretty(&request_payload).unwrap());
 
-    let client = reqwest::Client::new();
+        let client = reqwest::Client::new();
 
-    let response: ChatResponse = client
-        .post("http://127.0.0.1:1234/v1/chat/completions")
-        .json(&request_payload)
-        .send()
-        .await?
-        .json()
-        .await?;
+        let response: ChatResponse = client
+            .post("http://127.0.0.1:1234/v1/chat/completions")
+            .json(&request_payload)
+            .send()
+            .await?
+            .json()
+            .await?;
 
 
-    let raw_content = &response.choices[0].message.content;
-    println!("{}", raw_content);
+        let raw_content = &response.choices[0].message.content;
+        println!("{}", raw_content);
+    };
 
     Ok(())
 }
---
+++ src/schemas.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+pub struct ChatResponse {
+    pub choices: Vec<ChatChoice>,
+}
+
+#[derive(Deserialize)]
+pub struct ChatChoice {
+    pub message: ChatMessage,
+}
+
+#[derive(Deserialize)]
+pub struct ChatMessage {
+    pub content: String,
+}
+
+#[derive(Serialize)] // to be able to convert to JSON
+pub struct UserMessage {
+    pub role: String,
+    pub content: String,
+}
