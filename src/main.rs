0234cd7d8a265d9b1db03ece6db5917d4a229641
daniel-mosko <danielmosko5@gmail.com>
2025-06-26 15:47:04 +0200

    refactor: Modularize API request handling
    Separate functions for handling Gemini and OpenAI API requests,
    improving code organization and readability. It also includes minor code
    improvements and adds necessary imports.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,9 @@ use dotenv::dotenv;
 use serde_json::json;
 use std::{env, error::Error};
 
-use crate::schemas::GeminiResponse;
 use crate::{
     git::{diff, get_changed_files, open_repo},
-    schemas::{ChatResponse, UserMessage},
+    schemas::{ChatResponse, GeminiResponse, UserMessage},
 };
 
 mod git;
@@ -13,6 +12,7 @@ mod schemas;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // if path not provided, use current working dir
     let repo_path = env::args().nth(1).unwrap_or_else(|| {
         env::current_dir()
             .expect("Failed to get current directory")
@@ -43,36 +43,46 @@ async fn main() -> Result<(), Box<dyn Error>> {
             content: diff(&repo, &files).unwrap(),
         };
 
-        let mut messages: Vec<UserMessage> = vec![];
-        messages.push(system_msg);
-        messages.push(send_msg);
+        let messages: Vec<UserMessage> = vec![system_msg, send_msg];
 
         let client = reqwest::Client::new();
-
         if use_gemini {
-            let msgs = messages
-                .iter()
-                .map(|msg| msg.content.as_str())
-                .collect::<Vec<_>>()
-                .join("\n");
+            handle_gemini_request(&client, &messages).await?;
+        } else {
+            handle_openai_request(&client, &messages).await?;
+        }
+        // println!("Sending JSON:\n{}", serde_json::to_string_pretty(&request_payload).unwrap());
+    }
+    Ok(())
+}
 
-            let request_payload = json!({
-                  "contents": [
-                    {
-                      "parts": [
-                        {
-                          "text": msgs
-                        }
+async fn handle_gemini_request(
+    client: &reqwest::Client,
+    messages: &[UserMessage],
+) -> Result<(), Box<dyn Error>> {
+    let msgs = messages
+        .iter()
+        .map(|msg| msg.content.as_str())
+        .collect::<Vec<_>>()
+        .join("\n");
 
-                      ]
-                    }
-                  ],
-            });
+    let request_payload = json!({
+          "contents": [
+            {
+              "parts": [
+                {
+                  "text": msgs
+                }
 
-            dotenv().ok();
-            let api_key = env::var("GEMINI_API_KEY")?;
+              ]
+            }
+          ],
+    });
 
-            let response: GeminiResponse= client
+    dotenv().ok();
+    let api_key = env::var("GEMINI_API_KEY")?;
+
+    let response: GeminiResponse= client
                 .post(format!("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={}",api_key))
                 .json(&request_payload)
                 .send()
@@ -80,47 +90,48 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 .json()
                 .await?;
 
-            let raw_response = &response.candidates[0].content.parts[0].text;
-            println!("{}", raw_response);
-        } else {
-            let request_payload = json!({
-                "model": "{{model}}",
-                "messages": messages,
-                "response_format": {
-                    "type": "json_schema",
-                    "json_schema": {
-                        "name": "commit_response",
-                        "strict": "true",
-                        "schema": {
-                            "type": "object",
-                            "properties": {
-                                "message": {
-                                    "type": "string"
-                                }
-                            },
-                            "required": ["message"]
+    let raw_response = &response.candidates[0].content.parts[0].text;
+    println!("{}", raw_response);
+    Ok(())
+}
+
+async fn handle_openai_request(
+    client: &reqwest::Client,
+    messages: &[UserMessage],
+) -> Result<(), Box<dyn Error>> {
+    let request_payload = json!({
+        "model": "{{model}}",
+        "messages": messages,
+        "response_format": {
+            "type": "json_schema",
+            "json_schema": {
+                "name": "commit_response",
+                "strict": "true",
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "message": {
+                            "type": "string"
                         }
-                    }
-                },
-                "temperature": 0.7,
-                "max_tokens": 50,
-                "stream": false
-            });
+                    },
+                    "required": ["message"]
+                }
+            }
+        },
+        "temperature": 0.7,
+        "max_tokens": 50,
+        "stream": false
+    });
 
-            let response: ChatResponse = client
-                .post("http://127.0.0.1:1234/v1/chat/completions")
-                .json(&request_payload)
-                .send()
-                .await?
-                .json()
-                .await?;
+    let response: ChatResponse = client
+        .post("http://127.0.0.1:1234/v1/chat/completions")
+        .json(&request_payload)
+        .send()
+        .await?
+        .json()
+        .await?;
 
-            let raw_response = &response.choices[0].message.content;
-            println!("{}", raw_response);
-        }
-
-        // println!("Sending JSON:\n{}", serde_json::to_string_pretty(&request_payload).unwrap());
-    };
-
+    let raw_response = &response.choices[0].message.content;
+    println!("{}", raw_response);
     Ok(())
 }
